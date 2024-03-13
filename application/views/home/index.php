94ed634d31759d76a621df32e9cce41196421292
Your Name <kananazera@gmail.com>
2024-03-14 00:33:38 +0400

    Created

---
+++ application/views/home/index.php
@@ -0,0 +1,3 @@
+<div class="container" data-aos="zoom-in-up">
+	Hello
+</div>
---
+++ application/views/layouts/footer.php
@@ -0,0 +1,47 @@
+<footer class="mt-5 bg-dark text-white py-3 px-5">
+
+    <div class="row">
+        <div class="col-12 col-md-4 mb-4 mb-md-0">
+            <div class="mb-3">
+                <a href="#">
+                    <img id="logo-footer" class="img-fluid" src="assets/img/logo-light.png" alt="">
+                </a>
+            </div>
+            <div>translate.footer_slogan</div>
+        </div>
+
+        <div class="col-12 col-md-4 mb-4 mb-md-0 justify-content-md-center d-flex">
+            <ul>
+                <li class="mb-2"><a href=""><i class="bi bi-arrow-right-short"></i> translate.categories</a></li>
+                <li class="mb-2"><a href=""><i class="bi bi-arrow-right-short"></i> translate.categories</a></li>
+                <li class="mb-2"><a href=""><i class="bi bi-arrow-right-short"></i> translate.about</a></li>
+                <li class="mb-2"><a href=""><i class="bi bi-arrow-right-short"></i> translate.contact</a></li>
+                <li class="mb-2"><a href=""><i class="bi bi-arrow-right-short"></i> translate.privacy_policy</a></li>
+                <li><a href=""><i class="bi bi-arrow-right-short"></i> translate.terms_and_conditions</a></li>
+            </ul>
+        </div>
+
+        <div class="col-12 col-md-4 text-center text-md-end">
+            <div class="mb-3">
+                <a target="_blank" class="mx-1" href="facebook_url"><i class="bi bi-facebook"></i></a>
+                <a target="_blank" class="mx-1" href="instagram_url"><i class="bi bi-instagram"></i></a>
+                <a target="_blank" class="mx-1" href="twitter_url"><i class="bi bi-twitter"></i></a>
+                <a target="_blank" class="mx-1" href="linkedin_url"><i class="bi bi-linkedin"></i></a>
+                <a target="_blank" class="mx-1" href="youtube_url"><i class="bi bi-youtube"></i></a>
+                <a target="_blank" class="mx-1" href="whatsapp_url"><i class="bi bi-whatsapp"></i></a>
+            </div>
+            <div>Ⓒ appname <?= date('Y') ?></div>
+        </div>
+    </div>
+
+</footer>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+		integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+		crossorigin="anonymous"></script>
+<script src="assets/js/aos.min.js"></script>
+<script>
+	AOS.init();
+</script>
+</body>
+</html>
